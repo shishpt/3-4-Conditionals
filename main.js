e9a485f98850486933363097126cb096afef06ce
Shishir Patel <shishirpatel@gmail.com>
2022-09-27 21:37:46 +0000

    update of main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const info = [{name:"Worthington Semor",birth_date:"11/22/1988",children:0,country:"Poland",program:false};
-{name:"Maddalena Heinzler",birth_date:"2/26/2000",children:3,country:"Slovenia",program:false};
-{name:"Mirilla Saterweyte",birth_date:"11/7/1984",children:2,country:"Brazil",program:true};
-{name:"Audy Dark",birth_date:"4/18/1989",children:5,country:"China",program:true};
-{name:"Taffy Grzegorzewski",birth_date:"3/6/1995",children:4,country:"China",program:false}];
+// Setup data
 
-function begin() {
-    document.getElementById("first").innerHTML = "test";1
-}
+const info = [{"name":"Worthington Semor","birth_date":"11/22/1988",children:0,country:"Poland",program:false},
+{name:"Maddalena Heinzler",birth_date:"2/26/2000",children:3,country:"Slovenia",program:false},
+{name:"Mirilla Saterweyte",birth_date:"11/7/1984",children:2,country:"Brazil",program:true},
+{name:"Audy Dark",birth_date:"4/18/1989",children:5,country:"China",program:true},
+{name:"Taffy Grzegorzewski",birth_date:"3/6/1995",children:4,country:"China",program:false}]
 
+    // Test
 console.log(info);
+
