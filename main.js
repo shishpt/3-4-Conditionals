ffd1344383ee6069863783e3da6e800a68ace0d0
Shishir Patel <shishirpatel@gmail.com>
2022-10-05 12:35:50 +0000

    update

--- main.js
+++ main.js
@@ -7,24 +7,45 @@ const info = [{"name": "Worthington Semor","birth_date": "11/22/1988","children"
 {"name":"Taffy Grzegorzewski","birth_date":"3/6/1995","children":4,"country":"China","program":false}]
 
     // Test
-console.log(info);
-
-
+const list = JSON.stringify(info);
 
 //Print data to screen
-let tData = info.length;
+let tData = list.length;
 let text = "<ul>";
 for (let i =0; i < tData; i++) {
-  text += "<li>" + info[i] + "</li>";
+  text += "<li>" + list[i] + "</li>";
 }
 text += "</ul>";
 
-document.getElementById("first").innerHTML = text;
-
 // Is the first person older than the last person?
+// Setup first person
+const fName = info[0].name;
+const fBDate = new Date(info[0].birth_date);
+let tDate = new Date();
+const fMSDiff = tDate - fBDate;
+const fAge = Math.floor(fMSDiff / (365.25*24*60*60*1000));
+document.getElementById("first").innerHTML = fName " is " fAge " years old.";
 
 
+// Setup third person
+const tName = info[2].name;
+const tBDate = new Date(info[2].birth_date);
+const tMSDiff = tDate - tBDate;
+const tAge = Math.floor(tMSDiff / (365.25*24*60*60*1000));
+
+document.getElementById("second").innerHTML = text;
+
+// Who is older?
+//if
+//  (fAge > tAge) {
+//    document.getElementById("older").innerHTML = "1"
+//} else if {
+//  (fAge == tAge) {
+//    document.getElementById("older").innerHTML = "both"
+//  }
+//else {
+//  document.getElementById("older").innerHTML = "3"
+//}
+//};
 
 // Does the 2nd person have the same amount of kids as the 3rd?
-
-
