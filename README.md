8f8196a9b6655b04ca6bbcaf3f0978258e410f97
Shishir Patel <shishirpatel@gmail.com>
2022-11-01 12:43:08 +0000

    updates all files for final push

--- README.md
+++ README.md
@@ -9,3 +9,16 @@
         **Ternary operator
     *Switch
         **break
+
+### ASSIGNMENT
+
+    Using data from https://api.mockaroo.com/api/6352abc0?count=5&key=632d7f80:
+      *Create a Javascript file that describes the data using the appropriate data types
+      *In the same file, making sure each person is represented by an object in an array:
+        **Is the first person older than the last person?
+        **Does the 2nd person have the same amount of kids as the 3rd?
+      *Create a statement that checks if the 1st person and the 4th person in the array both know how to program. If yes, output “Yay!”, if not output “LMGTFY”.
+      *Create a statement that checks the 2nd person in the array for their nationality. If the person’s country is Iceland, output “Hæ”, if he/she is from Spain output “Hola”, if he/she is from Korea output “여보세요”). If the country is none of the above, output “Hello”. Tip: a switch statement might be a good fit for this task.
+      *Use a ternary operator to output if the 2nd person’s name is longer than 5 characters.
+
+#### THIS ASSIGNMENT HAS BEEN SPLIT BETWEEN THE DOM AND THE CONSOLE LOG.
--- main.js
+++ main.js
@@ -12,6 +12,7 @@ for (var {name: n, birth_date: b, children: c, country: o, program: p} of info) 
 };
 
 // Is the first person older than the last person?
+//--INCLUDED DOM MANUIPULATION--
 // Setup first person
 const fName = info[0].name;
 const fBDate = new Date(info[0].birth_date);
@@ -19,7 +20,6 @@ let tDate = new Date();
 const fMSDiff = tDate - fBDate;
 const fAge = Math.floor(fMSDiff / (365.25*24*60*60*1000));
 document.getElementById("first").innerHTML = fName + " is " + fAge + " years of age.";
-
 
 // Setup third person
 const tName = info[2].name;
@@ -31,10 +31,12 @@ document.getElementById("second").innerHTML = tName + " is " + tAge + " years of
 
 // Who is older?
 if (fAge < tAge) {
-  console.log(fName + " is older")
+//  console.log(fName + " is older")
+  document.getElementById("older").innerHTML =fName + " is older than " + tName
 }
 else {
-  console.log(tName + " is older");
+//  console.log(tName + " is older");
+  document.getElementById("older").innerHTML =tName + " is older than " + fName
 }
 
 // Does the 2nd person have the same amount of kids as the 3rd?
@@ -44,12 +46,12 @@ const sKid = info[1].children;
 const tKid = info[2].children;
 
 if (sKid === tKid) {
-//  console.log(sName + " " + tName + " have the same amount of children." )
+    console.log(sName + " " + tName + " have the same amount of children." )
     document.getElementById("k1").innerHTML = fName + " and " + tName + " have the same amount of children."
 }
 else  {  
-//  console.log(tName + " and " + sName + " do not have the same amount of children.")
-    document.getElementById("k1").innerHTML = fName + " and " + tName + " do not have the same amount of children."
+    console.log(tName + " and " + sName + " do not have " + "<br>" + "the same amount of children.")
+    document.getElementById("k1").innerHTML = fName + " and " + tName + " do not " + "<br>" + "have the same amount of children."
 };
 
 //  1st and 4th can program 
@@ -68,18 +70,23 @@ else {
 const sCountry = info[1].country;
 if (sCountry == "Iceland") {
   console.log("Hæ")
+  document.getElementById("nationality").innerHTML = "Hæ"
 }
 else if (sCountry == "Spain") {
   console.log("Hola")
+  document.getElementById("nationality").innerHTML = "Hola"
 }
 else if (sCountry == "Korea") {
   console.log("여보세요")
+  document.getElementById("nationality").innerHTML = "여보세요"
 }
 else {
   console.log("Hello")
+  document.getElementById("nationality").innerHTML = "hello"
 }
 
 //Tertiary operation checking to see if 2nd person has 5 characters in their name
 
 let sLName = (sName.length < 5) ? "Less than 5":"More than 5";
-console.log(sLName);
+  console.log(sLName);
+  document.getElementById("nameLength").innerHTML = sLName
